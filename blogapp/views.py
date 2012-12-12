c02da54b9c71b2327e9493902aea73c292dc634a
OpenShift guest <benvnguyen@gmail.com>
2012-12-12 15:19:17 -0500

    changed views again

--- blogapp/views.py
+++ blogapp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 def hello_world(request):
     x=1234
     import code; code.interact(local=locals())
-    return HttpResponse("hello world, from brian!!")
+    return HttpResponse("hello world, from philip!!")
     
